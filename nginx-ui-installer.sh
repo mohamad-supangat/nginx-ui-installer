c8c8dc79c5b54ac63b8e81ebd55868241d39fe6a
Mohamad Supangat <hantamkoding@gmail.com>
2022-08-21 00:38:40 +0700

    fixing

--- nginx-ui-installer.sh
+++ nginx-ui-installer.sh
@@ -1,59 +1,11 @@
+
 REPO=0xJacky/nginx-ui
 FILES=(
     'frontend-dist.tar.gz'
     'nginx-ui-linux-arm64-v8a.tar.gz'
 )
 
-# function Extract for common file formats
-SAVEIFS=$IFS
-IFS="$(printf '\n\t')"
-
-function extract {
- if [ -z "$1" ]; then
-    # display usage if no parameters given
-    echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz|.zlib>"
-    echo "       extract <path/file_name_1.ext> [path/file_name_2.ext] [path/file_name_3.ext]"
- else
-    for n in "$@"
-    do
-      if [ -f "$n" ] ; then
-          case "${n%,}" in
-            *.cbt|*.tar.bz2|*.tar.gz|*.tar.xz|*.tbz2|*.tgz|*.txz|*.tar)
-                         tar xvf "$n"       ;;
-            *.lzma)      unlzma ./"$n"      ;;
-            *.bz2)       bunzip2 ./"$n"     ;;
-            *.cbr|*.rar) unrar x -ad ./"$n" ;;
-            *.gz)        gunzip ./"$n"      ;;
-            *.cbz|*.epub|*.zip) unzip ./"$n"       ;;
-            *.z)         uncompress ./"$n"  ;;
-            *.7z|*.apk|*.arj|*.cab|*.cb7|*.chm|*.deb|*.dmg|*.iso|*.lzh|*.msi|*.pkg|*.rpm|*.udf|*.wim|*.xar)
-                         7z x ./"$n"        ;;
-            *.xz)        unxz ./"$n"        ;;
-            *.exe)       cabextract ./"$n"  ;;
-            *.cpio)      cpio -id < ./"$n"  ;;
-            *.cba|*.ace) unace x ./"$n"      ;;
-            *.zpaq)      zpaq x ./"$n"      ;;
-            *.arc)       arc e ./"$n"       ;;
-            *.cso)       ciso 0 ./"$n" ./"$n.iso" && \
-                              extract $n.iso && \rm -f $n ;;
-            *.zlib)      zlib-flate -uncompress < ./"$n" > ./"$n.tmp" && \
-                              mv ./"$n.tmp" ./"${n%.*zlib}" && rm -f "$n"   ;;
-            *)
-                         echo "extract: '$n' - unknown archive method"
-                         return 1
-                         ;;
-          esac
-      else
-          echo "'$n' - file does not exist"
-          return 1
-      fi
-    done
-fi
-}
-
-IFS=$SAVEIFS
-
-
+mkdir frontend
 
 for FILE in ${FILES[@]}; do
     # hapus file lama
@@ -65,11 +17,10 @@ for FILE in ${FILES[@]}; do
     | cut -d : -f 2,3 \
     | tr -d \" \
     | wget -i -
-
-    extract $FILE
 done
 
-
-cp nginx-ui /usr/bin/nginx-ui
-dinitctl restart nginx-ui
+tar -zxf frontend-dist.tar.gz -C frontend
+tar -zxf 'nginx-ui-linux-arm64-v8a.tar.gz'
+sudo cp nginx-ui /usr/bin/nginx-ui
+sudo dinitctl restart nginx-ui
 
